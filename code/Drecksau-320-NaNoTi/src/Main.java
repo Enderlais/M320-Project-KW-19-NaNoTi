a404f3bd81e4c9ea8b863564896f354d1495f503
Noah Menke <noahmenke2007@gmail.com>
2026-05-08 11:33:59 +0200

    karten und Schweichen klasse hinzugefuegt

--- code/Drecksau-320-NaNoTi/src/Main.java
+++ code/Drecksau-320-NaNoTi/src/Main.java
@@ -1,9 +1,27 @@
-
+ import java.util.ArrayList;
+ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		
         System.out.println("Wilkommen zu unserem dreckssau text spiel");
+        
+        while(true) {
+        	System.out.println("wollen sie eine runde spielen");
+        	System.out.println("0 = nein");
+        	System.out.println("1 = ja");
+        	
+        	int awn = 0;
+        	awn = sc.nextInt();
+        	
+        	if(awn == 1) {
+        		System.out.println("okaz");
+        	}
+        	
+        }
+        
 	}
 
 }
