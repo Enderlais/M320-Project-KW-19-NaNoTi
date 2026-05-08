f50bc7a6368af2eda421d49b852beb363ea64ace
Natmich <natanmichael27@gmail.com>
2026-05-08 11:13:56 +0200

    Update Main.java

--- code/Drecksau-320-NaNoTi/src/Main.java
+++ code/Drecksau-320-NaNoTi/src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-	System.out.println("Wilkommen zu unserem dreckssau text spiel");
+
+        System.out.println("Wilkommen zu unserem dreckssau text spiel");
 	}
 
 }
