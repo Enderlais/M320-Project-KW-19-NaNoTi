005e54afd67ed37c9657cd9beef2dc5598ea7217
Noah Menke <noahmenke2007@gmail.com>
2026-05-11 09:11:16 +0200

    karten.java basic layout, getter und setter

--- code/Drecksau-320-NaNoTi/src/Karten.java
+++ code/Drecksau-320-NaNoTi/src/Karten.java
@@ -1,4 +1,34 @@
 
 public class Karten {
+	private String Name;
+	private int Stuck;
 
+	
+	
+	Karten(String Name, int Stuck) {
+		this.Name = Name;
+		this.Stuck = Stuck;
+		}
+	
+	String GetName() {
+		return Name;
+		
+	}
+	
+	int GetStuck() {
+		return Stuck;
+		
+	}
+	
+	
+	void setName(String Na) {
+		Name = Na;
+		
+	}
+	
+	void setStuck(int r) {
+		r = Stuck;
+		
+	}
 }
+
