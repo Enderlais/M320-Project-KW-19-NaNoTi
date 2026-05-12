f59a358a0c5c7ac919e61b144a9da17bab67c9e6
Natmich <natanmichael27@gmail.com>
2026-05-12 14:24:57 +0200

    Update Main.java und Spieler.ĵava erstellt

---
+++ code/Drecksau-320-NaNoTi/src/Spieler.java
@@ -0,0 +1,20 @@
+public class Spieler {
+
+    private String spname;
+
+
+
+
+    Spieler(String spname) {
+        this.spname = spname;
+
+    }
+
+    String Getspname() {
+        return spname;
+
+    }
+
+    public String printspieler(String spnList) {return spname;}
+
+}
