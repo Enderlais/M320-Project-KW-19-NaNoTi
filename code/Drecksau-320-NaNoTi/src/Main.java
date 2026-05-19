07cf8a111a42e5519962de27bda2316cf028a0ef
Tim Schlosser08 <tim.schlosser14@gmail.com>
2026-05-19 13:30:47 +0200

    Toller Titel als test

--- code/Drecksau-320-NaNoTi/src/Main.java
+++ code/Drecksau-320-NaNoTi/src/Main.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 import static java.lang.Thread.sleep;
-
+//=================================================================================
+//                      Main Methode
+//=================================================================================
 public class Main {
 
     public static void main(String[] args) {
