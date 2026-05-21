5364b5fefa2b847e965f9f878acb38246b497a59
Natmich <natanmichael27@gmail.com>
2026-05-21 10:21:38 +0200

    Update Spieler.java

--- code/Drecksau-320-NaNoTi/src/Spieler.java
+++ code/Drecksau-320-NaNoTi/src/Spieler.java
@@ -9,30 +9,32 @@ public class Spieler {
     }
 
     public void InitHand(KartenDeck deck) {
-    	
-    	 ArrayList<Karten> hand = new ArrayList<>();
-    	 
-    	 while(hand.size() != 3) {
-             hand.add(deck.ziehen());                   //geht doch
-    	 }
-    }
-    
-    public void Initschweichen() {
-    	
-   	 ArrayList<Karten> sps = new ArrayList<>();                 //sps ist spieler schweinchen
-   	 
-   	 if(spa = 2 ) {
-   	 while(sps.size() != 5) {
-            sps.add(new Schweinchen(false, false, false, false ));                   //geht doch
-   	 }
-   	 }
-    
-    String Getspname() {
-        return spname;
+
+        ArrayList<Karten> hand = new ArrayList<>();
+
+        while (hand.size() != 3) {
+            hand.add(deck.ziehen());                   //geht doch
+        }
     }
 
-    @Override
-    public String toString() {
-        return spname;
+    public void Initschweichen() {
+
+        ArrayList<Karten> sps = new ArrayList<>();                 //sps ist spieler schweinchen
+
+        if (spa = 2) {
+            while (sps.size() != 5) {
+                sps.add(new Schweinchen(false, false, false, false));                   //geht doch
+            }
+        }
+    }
+
+        String Getspname () {
+            return spname;
+        }
+
+        @Override
+        public String toString () {
+            return spname;
+        }
     }
 }
