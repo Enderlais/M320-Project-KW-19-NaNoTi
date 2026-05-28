e42eb2633d1cb6cf12626179c0f1b2b3310cedb0
Noah Menke <noahmenke2007@gmail.com>
2026-05-28 15:13:17 +0200

    mehr logik

--- code/Drecksau-320-NaNoTi/src/RegenKarte.java
+++ code/Drecksau-320-NaNoTi/src/RegenKarte.java
@@ -6,9 +6,16 @@ public class RegenKarte extends Karten {
 
     @Override
     public void benutzen(Spieler spieler, int swn) {                                    //swn = schweinenummer 			//geht ned, weiss nicht wiso
-        Schweinchen jsw = spieler.sps.get(swn);
-        jsw.SetDrecking(false);    // dann set dreckig auf 0							//jws ist jetztige schwein nummer
-        spieler.sps.set(swn, jsw);
+       swn = 0;
+    	while(spieler.sps.size() != swn) {
+    	Schweinchen jsw = spieler.sps.get(swn);
+        if(jsw.GetHausbesitzer() == false) {
+        	jsw.SetDrecking(false);    // dann set dreckig auf 0							//jws ist jetztige schwein nummer
+        } else {System.out.println("leider hatt schweinchen nummer " + swn + " von " + spieler + " ein haus.");
+        } swn++;
+        
+    	}
+        
         System.out.println("Ein Schwein wurde dreckig!");
     }
 
